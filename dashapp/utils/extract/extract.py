d4fdb26ab7d245a29ac4b6576a3272ce272f6b37
gavh3 <gavin.hcm@gmail.com>
2023-09-24 11:21:12 -0700

    commit

--- dashapp/utils/extract/extract.py
+++ dashapp/utils/extract/extract.py
@@ -2,44 +2,44 @@ import datetime
 import json
 import os
 import requests
-from google.oauth2 import service_account
-from google.cloud import storage
+# from google.oauth2 import service_account
+# from google.cloud import storage
 from io import BytesIO
 from PIL import Image
 
-service_account_credentials = service_account.Credentials.from_service_account_file('secrets/google_credentials/google_storage_reader_credentials.json')
-client = storage.Client(credentials=service_account_credentials)
-bucket = client.get_bucket('nhl-stats-raw')
+# service_account_credentials = service_account.Credentials.from_service_account_file('secrets/google_credentials/google_storage_reader_credentials.json')
+# client = storage.Client(credentials=service_account_credentials)
+# bucket = client.get_bucket('nhl-stats-raw')
 
-def get_play_by_play(year=None, game_type=None):
-    prefix = 'games/year={}/game_type={}/'.format(year, game_type)
-    blobs = bucket.list_blobs(prefix=prefix)
+# def get_play_by_play(year=None, game_type=None):
+#     prefix = 'games/year={}/game_type={}/'.format(year, game_type)
+#     blobs = bucket.list_blobs(prefix=prefix)
 
-    with open('data/play_by_play.json', 'w') as outfile:
-        for blob in blobs:
-            # read each blob into memory as a string
-            blob_string = blob.download_as_string()
-            # parse the string into a python dictionary
-            blob_dict = json.loads(blob_string)
-            # write the dictionary to a file
-            json.dump(blob_dict, outfile)
-            # write a new line
-            outfile.write('\n')
+#     with open('data/play_by_play.json', 'w') as outfile:
+#         for blob in blobs:
+#             # read each blob into memory as a string
+#             blob_string = blob.download_as_string()
+#             # parse the string into a python dictionary
+#             blob_dict = json.loads(blob_string)
+#             # write the dictionary to a file
+#             json.dump(blob_dict, outfile)
+#             # write a new line
+#             outfile.write('\n')
 
-def get_players(year=None):
-    prefix = 'players/year={}/'.format(year)
-    blobs = bucket.list_blobs(prefix=prefix)
+# def get_players(year=None):
+#     prefix = 'players/year={}/'.format(year)
+#     blobs = bucket.list_blobs(prefix=prefix)
 
-    with open('data/players.json', 'w') as outfile:
-        for blob in blobs:
-            # read each blob into memory as a string
-            blob_string = blob.download_as_string()
-            # parse the string into a python dictionary
-            blob_dict = json.loads(blob_string)
-            # write the dictionary to a file
-            json.dump(blob_dict, outfile)
-            # write a new line
-            outfile.write('\n')
+#     with open('data/players.json', 'w') as outfile:
+#         for blob in blobs:
+#             # read each blob into memory as a string
+#             blob_string = blob.download_as_string()
+#             # parse the string into a python dictionary
+#             blob_dict = json.loads(blob_string)
+#             # write the dictionary to a file
+#             json.dump(blob_dict, outfile)
+#             # write a new line
+#             outfile.write('\n')
 
 def get_api(url):
     try:
